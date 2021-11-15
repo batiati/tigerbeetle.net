edea85ad12a54e1f8a329d72deaeeb8342e779a7
batiati <rbatiati@gmail.com>
2021-11-15 14:42:47 -0300

    Install script

--- scripts/install_dotnet.sh
+++ scripts/install_dotnet.sh
@@ -2,5 +2,6 @@
 set -e
 
 curl --silent --progress-bar --output dotnet-install.sh "https://dotnet.microsoft.com/download/dotnet/scripts/v1/dotnet-install.sh"
+chmod +x dotnet-install.sh
 ./dotnet-install.sh --version 5.0.209 --install-dir ./dotnet
 rm ./dotnet-install.sh
